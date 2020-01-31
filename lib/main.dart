e6b9b8d81c3b846fa07d668f61856dcec5f522fa
Nisan Sabag <nisans@google.com>
2020-01-31 23:01:18 +0200

    adding the app

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/nisan.png'),
+            ),
+            Text(
+              'Nisan Sabag',
+              style: TextStyle(
+                  fontFamily: 'Raleway',
+                  fontSize: 36.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal),
+            ),
+            Text(
+              'WEB DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+972 525 644 177',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'nisan.sabag.com@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+} /*
+Row(
+children: <Widget>[
+Icon(
+Icons.email,
+color: Colors.teal,
+),
+SizedBox(
+width: 10.0,
+),
+Text(
+'nisan.sabag.com@gmail.com',
+style: TextStyle(
+color: Colors.teal.shade900,
+fontFamily: 'Source Sans Pro',
+fontSize: 20.0,
+),
+),
+],
+),*/
